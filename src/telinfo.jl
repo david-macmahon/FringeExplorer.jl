4ce175a6ee93f6fccef16eb65be3e54ff77a3fd1
David MacMahon <davidm@astro.berkeley.edu>
2022-12-04 02:36:07 -0800

    Add support for TOML telinfo files

--- src/telinfo.jl
+++ src/telinfo.jl
@@ -1,18 +1,34 @@
 # Functions related to telinfo.yml files
 
-using YAML: load_file
+import YAML: load_file
+using TOML: parse
 
 """
-    telinfo(yamlfile)
+    telinfo(telinfo_file; dicttype=Dict{Symbol,Any})
 
-Loads YAML file `yamlfile` using `dicttype=Dict{Symbol,Amy}`.
+Loads telescope information from `telinfo_file`.  If `telinfo_file` ends with
+`.yml` or `.yaml` it will be loaded as a YAML file using `dicttype`
+dictionaries.  If `telinfo_file` ends with `.toml` it will be loaded as a TOML
+file and `dicttype` will be ignored since TOML files always use
+`Dict{String,Any}`.
 """
-telinfo(fname) = load_file(fname, dicttype=Dict{Symbol,Any})
+function telinfo(fname; dicttype=Dict{Symbol,Any})
+    _, ext = splitext(fname)
+    if ext == ".yml" || ext == ".yaml"
+        load_file(fname; dicttype)
+    elseif ext == ".toml"
+        parse(fname)
+    else
+        error("""unknown extention: "$(ext)\"""")
+    end
+end
 
 """
     antpos(telinfo_yml::AbstracyString)
     antpos(telinfo::AbstractDict{Symbol, Any})
-    antpos(antennas::AbstractVector{<:AbstractDict{Symbol,Amy}})
+    antpos(telinfo::AbstractDict{String, Any})
+    antpos(antennas::AbstractVector{<:AbstractDict{Symbol,Any}})
+    antpos(antennas::AbstractVector{<:AbstractDict{String,Any}})
 
 Return antenna positions from `antennas` Vector as a 3xN matrix (i.e. one
 antenna per columm).  The antenna ordering is sorted by the `number` of each
@@ -36,10 +52,19 @@ function antpos(antennas::AbstractVector{<:AbstractDict{Symbol,Any}})
     reduce(hcat, (a[:position] for a in ants))
 end,
 
+function antpos(antennas::AbstractVector{<:AbstractDict{String,Any}})
+    ants = sort(antennas, by=a->a["number"])
+    reduce(hcat, (a["position"] for a in ants))
+end,
+
 function antpos(ti::AbstractDict{Symbol,Any})
     antpos(ti[:antennas])
 end,
 
-function antpos(tiyml::AbstractString)
-    antpos(telinfo(tiyml))
+function antpos(ti::AbstractDict{String,Any})
+    antpos(ti["antennas"])
+end,
+
+function antpos(tiyml::AbstractString; dicttype=Dict{Symbol,Any})
+    antpos(telinfo(tiyml; dicttype))
 end
